777c5021e0948d8625d9f0acf1afe446891300fd
Scott Lau <liuweigeek@outlook.com>
2021-06-14 23:09:14 +0800

    Initial Commit

---
+++ NeptuneBlog-iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NeptuneBlog-iOS
+//
+//  Created by Scott Lau on 2021/6/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NeptuneBlog-iOS/NeptuneBlog_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeptuneBlog_iOSApp.swift
+//  NeptuneBlog-iOS
+//
+//  Created by Scott Lau on 2021/6/14.
+//
+
+import SwiftUI
+
+@main
+struct NeptuneBlog_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
