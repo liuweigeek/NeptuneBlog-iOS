1a233ed1de1e6dccb09201d339388ae96afae6c1
Scott Lau <liuweigeek@outlook.com>
2021-06-26 16:21:44 +0800

    updated layouts

--- NeptuneBlog-iOS/View/Tweets/TweetCell.swift
+++ NeptuneBlog-iOS/View/Tweets/TweetCell.swift
@@ -9,65 +9,68 @@ import SwiftUI
 import Kingfisher
 
 struct TweetCell: View {
-
+    
     @Environment(\.colorScheme) var colorScheme
-
+    
     let tweet: Tweet
-
+    
     var body: some View {
         VStack(alignment: .leading) {
             HStack(alignment: .top, spacing: 12) {
                 if let smallAvatar = tweet.author.smallAvatar {
                     KFImage(URL(string: smallAvatar))
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 56, height: 56)
-                            .clipShape(Circle())
-                            .padding(.leading)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 56, height: 56)
+                        .clipShape(Circle())
+                        .padding(.leading)
                 } else {
                     Image(systemName: "person.crop.circle")
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 56, height: 56)
-                            .clipShape(Circle())
-                            .foregroundColor(.gray)
-                            .padding(.leading)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 56, height: 56)
+                        .clipShape(Circle())
+                        .foregroundColor(.gray)
+                        .padding(.leading)
                 }
-
-
+                
+                
                 VStack(alignment: .leading, spacing: 4) {
                     HStack {
                         Text(tweet.author.name)
-                                .font(.system(size: 14, weight: .semibold))
-                                .foregroundColor(colorScheme == .dark ? .white : .black)
-
+                            .font(.system(size: 13, weight: .semibold))
+                            .foregroundColor(colorScheme == .dark ? .white : .black)
+                        
                         Text("@\(tweet.author.username) •")
-                                .foregroundColor(.gray)
-
+                            .font(.system(size: 12))
+                            .foregroundColor(.gray)
+                        
                         Text(String(tweet.timestampString))
-                                .foregroundColor(.gray)
-
+                            .font(.system(size: 12))
+                            .foregroundColor(.gray)
+                        
                         Spacer()
-
+                        
                         Button(action: {}, label: {
                             Text("...")
-                                    .font(.system(size: 14, weight: .semibold))
-                                    .foregroundColor(.gray)
+                                .font(.system(size: 12, weight: .semibold))
+                                .foregroundColor(.gray)
                         })
-                                .padding(.trailing)
+                        .padding(.trailing, 2)
                     }
-
+                    
                     Text(tweet.text)
-                            .foregroundColor(colorScheme == .dark ? .white : .black)
+                        .foregroundColor(colorScheme == .dark ? .white : .black)
+                        .font(.system(size: 16))
                 }
             }
-                    .padding(.vertical)
-                    .padding(.trailing)
-
+            .padding(.vertical)
+            .padding(.trailing, 2)
+            
             TweetActionsView(tweet: tweet)
-
+            
             Divider()
         }
-                .padding(.leading, -16)
+        .padding(.leading, -16)
     }
 }
--- NeptuneBlog-iOS/View/Tweets/TweetDetailView.swift
+++ NeptuneBlog-iOS/View/Tweets/TweetDetailView.swift
@@ -9,9 +9,9 @@ import SwiftUI
 import Kingfisher
 
 struct TweetDetailView: View {
-
+    
     let tweet: Tweet
-
+    
     var body: some View {
         VStack(alignment: .leading, spacing: 16) {
             HStack {
@@ -29,52 +29,52 @@ struct TweetDetailView: View {
                         .foregroundColor(.gray)
                         .clipShape(Circle())
                 }
-
+                
                 VStack(alignment: .leading, spacing: 4) {
                     Text(tweet.author.name)
-                            .font(.system(size: 14, weight: .semibold))
-
+                        .font(.system(size: 14, weight: .semibold))
+                    
                     Text("@\(tweet.author.username)")
-                            .font(.system(size: 14))
-                            .foregroundColor(.gray)
+                        .font(.system(size: 12))
+                        .foregroundColor(.gray)
                 }
             }
-
+            
             Text(tweet.text)
-                    .font(.system(size: 22))
-
+                .font(.system(size: 22))
+            
             Text(tweet.detailedTimestampString)
-                    .font(.system(size: 14))
-                    .foregroundColor(.gray)
-
+                .font(.system(size: 14))
+                .foregroundColor(.gray)
+            
             Divider()
-
+            
             HStack(spacing: 12) {
                 HStack(spacing: 4) {
                     Text("0")
-                            .font(.system(size: 14, weight: .semibold))
-
+                        .font(.system(size: 14, weight: .semibold))
+                    
                     Text("Retweets")
-                            .font(.system(size: 14))
-                            .foregroundColor(.gray)
+                        .font(.system(size: 14))
+                        .foregroundColor(.gray)
                 }
-
+                
                 HStack(spacing: 4) {
                     Text("0")
-                            .font(.system(size: 14, weight: .semibold))
-
+                        .font(.system(size: 14, weight: .semibold))
+                    
                     Text("Likes")
-                            .font(.system(size: 14))
-                            .foregroundColor(.gray)
+                        .font(.system(size: 14))
+                        .foregroundColor(.gray)
                 }
             }
-
+            
             Divider()
-
+            
             TweetActionsView(tweet: tweet)
-
+            
             Spacer()
         }
-                .padding()
+        .padding()
     }
 }
